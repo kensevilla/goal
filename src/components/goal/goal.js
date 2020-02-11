715aad472e6f4b20b1a7e52d4adfa814c4c09c44
Ken Sevilla <ken.michael.sevilla@oocl.com>
2020-02-11 11:07:33 +0800

    add style in goal. refactor goal

--- src/components/goal/goal.js
+++ src/components/goal/goal.js
@@ -2,63 +2,84 @@ import React from 'react'
 import {connect} from "react-redux"
 import {bindActionCreators} from "redux"
 
+import 'antd/dist/antd.css'
+import { Row, Col, Button, Icon, Alert } from 'antd';
+
 import {completeGoal, failGoal, moveGoal} from '../../state/goal/action'
 import {showModal} from '../../state/main/action'
 import {getCurrentDate} from '../util/util'
 
+import './goal.css'
+
 class Goal extends React.Component{
-    state ={
-        newTargetDate: getCurrentDate()
-    }
 
     moveGoal = () => {
         let goalToBeMove = {
             id: this.props.goal.id,
             description: this.props.goal.description,
             targetDate: this.props.goal.targetDate,
-            newTargetDate: this.state.newTargetDate
         }
         this.props.actions.showModal(goalToBeMove)
+    }
+
+    prepareGoalBasedOnStatus = () =>{
+        let status  = this.props.goal.status;
+        let description = this.props.goal.description;
+        return(
+            status === 'Completed' ?
+                <Row>
+                    <Col span={12}>
+                        <Alert type="success" showIcon={false} message={description} banner />
+                    </Col>
+                    <Col span={7}>
+                        <Icon id='completedIcon' type="check" />
+                    </Col>
+                    <Col span={5}>
+                        <span>Original Date: {this.props.goal.targetDate}</span>
+                    </Col>
+                </Row>
+            : status === 'In-Progress' ?
+                <Row>
+                    <Col span={12}>
+                        <span id='goalDescription'>{description}</span>
+                        {this.props.goal.targetDate < getCurrentDate() && <Alert message="Failed to finish before target date. Either try again or give-up." banner />}
+                    </Col>
+                    <Col span={7}>
+                        {this.setActionsBasedOnDate()}
+                    </Col>
+                </Row>
+            :
+                <Row>
+                    <Col span={12}>
+                        <Alert type="error" showIcon={false} message={description} banner />
+                    </Col>
+                    <Col span={7}>
+                        <Icon id='failIcon' type="close" />
+                    </Col>
+                </Row>
+        )
     }
     
     setActionsBasedOnDate = () =>{
         let id = this.props.goal.id;
         return this.props.goal.targetDate >= getCurrentDate() ? 
             <div className='Action'>
-                <button onClick={this.props.actions.completeGoal.bind(this, id)}>Complete</button>
+                <Button id="completeButton"  shape="circle" onClick={this.props.actions.completeGoal.bind(this, id)}><Icon type="check" /></Button>
             </div>
             :
             <div className='Action'>
-                <button onClick={this.props.actions.failGoal.bind(this, id)}>Give-up</button>
-                <button onClick={this.moveGoal}>Move</button>
+                <Button id="failButton"  shape="circle" onClick={this.props.actions.failGoal.bind(this, id)}><Icon type="close" /></Button>
+                <Button id="moveButton"  shape="circle" onClick={this.moveGoal}><Icon type="redo" /></Button>
             </div>;
-    }
-
-    handleChange = (event) =>{
-        this.setState({
-            [event.target.name] : event.target.value
-        })
     }
 
     render(){
         let status  = this.props.goal.status;
-        let description = this.props.goal.description;
         let id = this.props.id;
         return(
-            status === 'Completed' ?
-                <div className={status} key={id}>
-                    <span>Completed: {description}</span> <br />
-                    <span>Original Target Date: {this.props.goal.targetDate}</span>
-                </div>
-            : status === 'In-Progress' ?
-                <div className={status} key={id}>
-                    <span>{description}</span>
-                    {this.setActionsBasedOnDate()}
-                </div>
-            :
-                <div className={status} key={id}>
-                    <span>Given up on: {description}</span>
-                </div>
+            <div className={status} key={id}>
+                {this.prepareGoalBasedOnStatus()}
+            </div>
         )
     }
 }
